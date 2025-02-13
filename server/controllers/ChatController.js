1d4c0a461a11e95bea72a33b4107116c9edec4cb
Tsedexashu08 <ashutsedex@gmail.com>
2025-02-13 08:15:55 -0900

    WTF!!

--- server/controllers/ChatController.js
+++ server/controllers/ChatController.js
@@ -48,16 +48,16 @@ export const StartChat = async (req, res) => {
         });
 
         if (chat) {
-            return res.status(200).json({ chat: chat.chat_id });
+            return res.status(200).json({ chat_id: chat.chat_id });
         } else {
             const newChat = await Chat.create({
                 user_id_1: user1Id,
                 user_id_2: user2Id
             });
-            return res.status(200).json({ chat: newChat.chat_id });
+            return res.status(200).json({ chat_id: newChat.chat_id });
         }
     } catch (error) {
-        console.error('Error in StartChat:', error.message); 
+        console.error('Error in StartChat:', error.message);
         return res.status(500).json({ error: error.message });
     }
 };
@@ -94,7 +94,7 @@ export const sendMessage = async (req, res) => {
 
         const newMessage = await Chat_Messages.create({
             chat_id: chatId,
-            sender_id: user1Id,
+            sender_id: user2Id,
             content: msgContent
         });
 
@@ -104,3 +104,18 @@ export const sendMessage = async (req, res) => {
         res.status(500).json({ error: error.message });
     }
 };
+
+export const FetchChatMessages = async (req, res) => {
+    try {
+        const {chatId} = req.body;
+        const messages = await Chat_Messages.findAll({
+            where: {
+                chat_id: chatId
+            }
+        });
+        res.status(200).json(messages);
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+
+}
--- server/routes/Chat.js
+++ server/routes/Chat.js
@@ -1,6 +1,6 @@
 import express from 'express'
 import sequelize from '../database.js'
-import { FetchUsers,FetchUser,FetchUsersWithChats,sendMessage,StartChat } from '../controllers/ChatController.js'
+import { FetchUsers,FetchUser,FetchUsersWithChats,sendMessage,StartChat,FetchChatMessages } from '../controllers/ChatController.js'
 
 const ChatRouter = express.Router()
 
@@ -9,5 +9,6 @@ ChatRouter.get('/getCurrentUser',FetchUsersWithChats)
 ChatRouter.post('/sendMessage',sendMessage)
 ChatRouter.post('/StartChat',StartChat)
 ChatRouter.post('/getUser',FetchUser)
+ChatRouter.post('/fecthMessages',FetchChatMessages)
 
 export default ChatRouter
